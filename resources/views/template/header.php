c65ae18e90e87dc01cfd3a0a546b33f8a7c7cba5
baptiste.nebout <baptiste16300@gmail.com>
2022-02-05 21:51:30 +0100

    ajout du include pour le template footer et header

---
+++ resources/views/template/header.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>cube 1</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+        <link href="../../css/app.css" rel="stylesheet">
+    </head>
+    <body class="body-main">
+        <div id="titre-logo">
+            <img src="https://d1fmx1rbmqrxrr.cloudfront.net/cnet/optim/i/edit/2019/04/eso1644bsmall__w770.jpg" id="logo">
+            <h1 id="titre-app">(RE)SOURECES RELMATIONNELLES</h1>
+        </div>
