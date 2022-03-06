1d265eb77b1e56dc4c9414011f3189be8eadabbd
Maëlle Dauphin <dau.maelle@gmail.com>
2022-03-06 18:29:01 +0100

    Ajout du seeder Categorie en fonction de la doc technique

--- database/seeders/CategorieSeeder.php
+++ database/seeders/CategorieSeeder.php
@@ -17,8 +17,47 @@ class CategorieSeeder extends Seeder
     public function run()
     {
         //
+        // Categorie::create([
+        //     'Nom' => 'test:'.Str::random(5),
+        // ]);
         Categorie::create([
-            'Nom' => 'test:'.Str::random(5),
+            'Nom' => "Communication",
+        ]);
+        Categorie::create([
+            'Nom' => "Cultures",
+        ]);
+        Categorie::create([
+            'Nom' => "Développement personnel",
+        ]);
+        Categorie::create([
+            'Nom' => "Intelligence émotionnelle",
+        ]);
+        Categorie::create([
+            'Nom' => "Loisirs",
+        ]);
+        Categorie::create([
+            'Nom' => "Monde professionnel",
+        ]);
+        Categorie::create([
+            'Nom' => "Parentalité",
+        ]);
+        Categorie::create([
+            'Nom' => "Qualité de vie",
+        ]);
+        Categorie::create([
+            'Nom' => "Recherche de sens",
+        ]);
+        Categorie::create([
+            'Nom' => "Santé physique",
+        ]);
+        Categorie::create([
+            'Nom' => "Santé psychique",
+        ]);
+        Categorie::create([
+            'Nom' => "Spiritualité",
+        ]);
+        Categorie::create([
+            'Nom' => "Vie affective",
         ]);
     }
 }
