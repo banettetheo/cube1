bedde549ad06343aad2c9af0e81b73024ddd7a1b
Maëlle Dauphin <dau.maelle@gmail.com>
2022-03-10 20:02:55 +0100

    Ajout seeder ressources

--- database/factories/RessourcesFactory.php
+++ database/factories/RessourcesFactory.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Factories\Factory;
 
 class RessourcesFactory extends Factory
 {
+    
     /**
      * Define the model's default state.
      *
@@ -14,7 +15,15 @@ class RessourcesFactory extends Factory
     public function definition()
     {
         return [
-            //
+            'Titre' => $this->faker->sentence(),
+            'Contenue' => $this->faker->text(200),
+            'Nombre_vue' => $this->faker->buildingNumber(),
+            'Nombre_like' => $this->faker->buildingNumber(),
+            'IdCategorie' => 1,
+            'IdType' => 1,
+            'IdUtilisateur_createur' => 2,
+            'IdEtat' => 1,
+            'Lien_ressources' => $this->faker->imageUrl(10,5)
         ];
     }
 }
--- database/seeders/RessourcesSeeder.php
+++ database/seeders/RessourcesSeeder.php
@@ -16,16 +16,17 @@ class RessourcesSeeder extends Seeder
      */
     public function run()
     {
-        Ressources::create([
-            'Lien_ressources' => 'Lien:'.Str::random(5),
-            'Titre' => 'Titre:'.Str::random(5),
-            'Contenue' => 'contenu:'.Str::random(5),
-            'Nombre_vue' => rand(1,999),
-            'Nombre_like' => rand(1,999),
-            'IdCategorie' => 1,
-            'IdType' => 1,
-            'IdUtilisateur_createur' => 2,
-            'IdEtat' => 1,
-        ]);
+        Ressources::factory(10)->create();
+        // Ressources::create([
+        //     'Lien_ressources' => 'Lien:'.Str::random(5),
+        //     'Titre' => 'Titre:'.Str::random(5),
+        //     'Contenue' => 'contenu:'.Str::random(5),
+        //     'Nombre_vue' => rand(1,999),
+        //     'Nombre_like' => rand(1,999),
+        //     'IdCategorie' => 1,
+        //     'IdType' => 1,
+        //     'IdUtilisateur_createur' => 2,
+        //     'IdEtat' => 1,
+        // ]);
     }
 }
