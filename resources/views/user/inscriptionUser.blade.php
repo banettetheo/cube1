fba05ec969d002309c27a21775c682c2429fee69
Maëlle Dauphin <dau.maelle@gmail.com>
2022-03-13 17:59:04 +0100

    Intialisation de l'enregistrement

--- resources/views/user/inscriptionUser.blade.php
+++ resources/views/user/inscriptionUser.blade.php
@@ -1,18 +1,20 @@
 <x-guest-layout>
 
     <section id="inscription-user" class="">
+    <x-auth-validation-errors class="mb-4" :errors="$errors" />
 
         <h1>Inscription</h1>
+        
+        <form id="inscription-form" method="POST" action="{{ route('register') }}">
+            @csrf
 
-        <form id="inscription-form">
+            <x-input type="text" id="name" name="name" class="input-inscription" placeholder="Nom" :value="old('name')" required autofocus />
+            <!-- <x-input type="text" id="prenom" name="prenom" class="input-inscription" placeholder="prenom" :value="old('prenom')" required autofocus /> -->
+            <x-input type="email" id="email" name="email" class="input-inscription" placeholder="Email" placeholder="email" :value="old('email')" required />
+            <x-input id="password" class="input-inscription" type="password" name="password" placeholder="Mot de passe" required autocomplete="new-password" />
+            <x-input id="password_confirmation" class="input-inscription" type="password" placeholder="Confirmer mot de passe" name="password_confirmation" required />
 
-            <input type="text" class="input-inscription" placeholder="nom"></input>
-            <input type="text" class="input-inscription" placeholder="prenom"></input>
-            <input type="text" class="input-inscription" placeholder="email"></input>
-            <input type="text" class="input-inscription" placeholder="mot de passe"></input>
-            <input type="text" class="input-inscription" placeholder="confirme mot de passe"></input>
-
-            <button class="btn-validation">inscription</button>
+            <x-button class="btn-validation">{{ __('S'inscription') }}</x-button>
 
         </form>
 
