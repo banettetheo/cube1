31baa8c51153940c11e42c1d1eb0afbda60e5e7d
Maëlle Dauphin <dau.maelle@gmail.com>
2022-03-19 22:27:26 +0100

    Affichage des ressources + Types de ressources

--- app/Http/Controllers/AccueilController.php
+++ app/Http/Controllers/AccueilController.php
@@ -9,6 +9,7 @@ use App\Models\Type_ressource;
 use App\Models\User;
 use App\Repositories\RessourceRepository;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
 
 class AccueilController extends Controller
 {
@@ -20,43 +21,64 @@ class AccueilController extends Controller
         $this->ressourceRepository = $ressourceRepository;
     }
 
-    public function index(){
+    public function index()
+    {
+        //Récupération des ressources
+        $request = Request::create('/api/ressources', 'GET', []);
+        $responseRessources = Route::dispatch($request)->getContent();
 
-        $lesRessources = $this->ressourceRepository->all();
+        //Récupération des types de ressource
+        $request = Request::create('/api/types-ressources', 'GET', []);
+        $responseTypesRessources = Route::dispatch($request)->getContent();
+
+        //Récupération des catégories
+        $request = Request::create('/api/types-ressources', 'GET', []);
+        $responseCategories = Route::dispatch($request)->getContent();
+
+
+
+        $lesRessources = json_decode($responseRessources, true);
+        $lesTypesRessources = json_decode($responseTypesRessources, true);
+        $lesCategories = json_decode($responseTypesRessources, true);
+
+
+
+
+
+
 
         $lesCategories = Categorie::all()
-        ->map(function ($categorie) {
-            return [
-                'id' => $categorie->id,
-                'nom' => $categorie->Nom
-            ];
-        });
+            ->map(function ($categorie) {
+                return [
+                    'id' => $categorie->id,
+                    'nom' => $categorie->Nom
+                ];
+            });
 
 
-        $lesTypesRessource = Type_ressource::all()
-            ->map(function ($typeRessource) {
-            return [
-                'id' => $typeRessource->id,
-                'nom' => $typeRessource->Nom
-            ];
-        });
+
         $lesUtilisateurs = User::all()
-        ->map(function ($utilisateur) {
-            return [
-                'id' => $utilisateur->id,
-                'nom' => $utilisateur->name,
-                'prenom' => $utilisateur->Prenom
-            ];
-        });
+            ->map(function ($utilisateur) {
+                return [
+                    'id' => $utilisateur->id,
+                    'name' => $utilisateur->name,
+                    'Prenom' => $utilisateur->Prenom
+                ];
+            });
+
+        $responsecode = 200;
+
+        $header = array(
+            'Content-Type' => 'application/json; charset=UTF-8',
+            'charset' => 'utf-8'
+        );
 
 
         return view('accueil', [
             'ressources' => $lesRessources,
             'categories' => $lesCategories,
-            'typesRessource' => $lesTypesRessource,
+            'typesRessources' => $lesTypesRessources,
             'utilisateurs' => $lesUtilisateurs
         ]);
     }
-
-
 }
