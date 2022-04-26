6084c0a219ea6fdd94ce82bb1333bb05641bd88e
Theyoyo09 <90200931+Theyoyo09@users.noreply.github.com>
2022-04-26 15:39:32 +0200

    TestUser fini

--- tests/Unit/UserTest.php
+++ tests/Unit/UserTest.php
@@ -36,18 +36,67 @@ class UserTest extends TestCase
        }
     }
 
+    public function test_readUser()
+    {
+        $user = User::factory(['id' => 99])->create();
+        $user->save();
+
+        if(User::find(99))
+        {
+            $this->assertTrue(true);
+        }
+        else
+        {
+            $this->assertTrue(false);
+        }
+    }
+
     public function test_deleteUser()
     {
-        User::find(1)->delete();
+        $user = User::factory(['id' => 1])->create();
+        echo $user;
+        $user->save();
+
+        try
+        {
+            User::find(1)->delete();
+            $this->assertTrue(true);
+        }
+        catch(RuntimeException $e)
+        {
+            $this->assertTrue(false);
+        }
     }
 
     public function test_updateUser()
     {
-        $this->assertTrue(false);
-    }
+        $user = User::factory(['id' => 1])->create();
+        echo $user;
+        $user->save();
 
-    public function test_readUser()
-    {
-        $this->assertTrue(false);
+        try
+        {
+            $user = User::find(1);
+
+            $user->name = "test";
+            $user->email = "test@gmail.com";
+            $user->Prenom = "Prenom_test";
+
+            $user->save();
+
+           if($user->name == "test" && $user->email == "test@gmail.com")
+           {
+            $this->assertTrue(true);
+           }
+           else
+           {
+            $this->assertTrue(false);
+           }
+            
+        }
+        catch(RuntimeException $e)
+        {
+            $this->assertTrue(false);
+        }
     }
 }
