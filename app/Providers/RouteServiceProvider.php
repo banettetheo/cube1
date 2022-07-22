4365f61e0003e5227449e8952e376574a6d087dd
Mawelle <dau.maelle@gmail.com>
2022-07-22 10:10:18 +0200

    Fix

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -17,7 +17,7 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    public const HOME = '/mon-compte';
+    public const HOME = '/mon-compte/consulter';
 
     /**
      * The controller namespace for the application.
